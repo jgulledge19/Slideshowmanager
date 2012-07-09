dd7ac83a210af9fe304f9487881acb5b55962943
BethelBoy <nathanhatfield@me.com>
2012-07-09 08:39:17 -0400

    modified:   assets/components/slideshowmanager/js/mgr/widgets/slide.grid.js 	modified:   core/components/slideshowmanager/lexicon/en/default.inc.php 	modified:   core/components/slideshowmanager/processors/mgr/slide/create.php 	modified:   core/components/slideshowmanager/processors/mgr/slide/getlist.php 	modified:   core/components/slideshowmanager/processors/mgr/slide/update.php

--- core/components/slideshowmanager/processors/mgr/slide/create.php
+++ core/components/slideshowmanager/processors/mgr/slide/create.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package CMP
- * @subpackage processors
+ * package CMP
+ * subpackage processors
  * This file needs to be customized
- */
+ 
 
 if (empty($scriptProperties['display_title'])) {
     $modx->error->addField('display_title',$modx->lexicon('suggestedsearch.account_err_ns_name'));
@@ -27,9 +27,48 @@ if ($modx->error->hasError()) {
 $suggestion = $modx->newObject('jgSearchSuggestions');
 $suggestion->fromArray($scriptProperties);
 
-/* save */
+/* save 
 if ($suggestion->save() == false) {
     return $modx->error->failure($modx->lexicon('suggestedsearch.account_err_save'));
 }
 
-return $modx->error->success('',$suggestion);
+return $modx->error->success('',$suggestion);*/
+/**
+ * @package CMP
+ * @subpackage processors
+ * This file needs to be customized
+ */
+
+if ( empty($scriptProperties['title']) ) {
+    $modx->error->addField('title',$modx->lexicon('slideshowmanager.slide_err_required'));
+}
+if ( empty($scriptProperties['description']) ) {
+    $modx->error->addField('description',$modx->lexicon('slideshowmanager.slide_err_required'));
+} 
+if ( empty($scriptProperties['start_date']) ) {
+    $modx->error->addField('start_date',$modx->lexicon('slideshowmanager.slide_err_required'));
+}
+if ( empty($scriptProperties['end_date']) ) {
+    $modx->error->addField('end_date',$modx->lexicon('slideshowmanager.slide_err_required'));
+}
+if ( empty($scriptProperties['slide_status']) ) {
+    $modx->error->addField('slide_status',$modx->lexicon('slideshowmanager.slide_err_required'));
+}
+if ( empty($scriptProperties['sequence'])  ) {
+    $modx->error->addField('sequence',$modx->lexicon('slideshowmanager.slide_err_required'));
+}
+
+
+if ($modx->error->hasError()) {
+    return $modx->error->failure();
+}
+
+$slides = $modx->newObject('jgSlideshowSlide');
+$slides->fromArray($scriptProperties);
+// 
+/* save */
+if ($slides->save() == false) {
+    return $modx->error->failure($modx->lexicon('slideshowmanager.slide_err_save'));
+}
+
+return $modx->error->success('',$slides);
